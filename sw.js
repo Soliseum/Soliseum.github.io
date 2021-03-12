6764bf560dd290a9e1b053f8173bfb2a56412e72
Christoph <34290975+Soliseum@users.noreply.github.com>
2021-03-12 15:16:00 -0800

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-a93f7e0f32c305721fc0.js"
+    "url": "webpack-runtime-cc90a2f8471a64e5c556.js"
   },
   {
     "url": "framework-dbb498007a7447f28d8e.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "7c70e6e76796729d33634cb934847f02"
+    "revision": "3eaf22d3c1661fdbf5a25455c46ab409"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-b0556ce5127c1a3e2490.js"
@@ -48,7 +48,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "971512a43e3a0262ec9abc1fa7252f5f"
+    "revision": "9ccd67050f28a7449b022c5214b10176"
   },
   {
     "url": "polyfill-e4957c11759bdfc149d9.js"
